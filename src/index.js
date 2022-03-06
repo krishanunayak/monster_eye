950eeb134767d4a265cf6595a237adbcb67949e7
Krishanu Nayak <krishanu.nayak@capgemini.com>
2022-03-06 15:24:40 +0530

    added The Eye with SVG

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import * as d3 from "d3";
+import { select, selectAll } from "d3-selection";
 
-d3.select("body").append("h1").text("hello world!");
+const [width, height] = [innerWidth, innerHeight];
+const [svgWidth, svgHeight] = [200, 200];
+const [eyeCenterX, eyeCenterY, eyeRadius] = [0, 0, svgWidth / 2];
+const eyeStrokeWidth = 5;
+
+const svg = select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr(
+        "transform",
+        `translate(${width / 2 - svgWidth / 2} ${height / 2 - svgHeight / 2})`
+    )
+    .attr(
+        "viewBox",
+        `${-svgWidth / 2} ${-svgHeight / 2} ${svgWidth} ${svgHeight}`
+    );
+
+svg.append("circle")
+    .attr("cx", eyeCenterX)
+    .attr("cy", eyeCenterY)
+    .attr("r", eyeRadius - eyeStrokeWidth)
+    .style("fill", "none")
+    .style("stroke", "black")
+    .style("stroke-width", eyeStrokeWidth);
